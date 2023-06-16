c32147795f9f7797ae83513bad9ff1ff41d25530
Andrew Loeliger <loeliger@wisc.edu>
2023-06-16 15:46:41 -0500

    update CICADA Input Network training script to most recent network status

--- scripts/CICADAInputNetworkScripts/cicadaInputNetwork.py
+++ scripts/CICADAInputNetworkScripts/cicadaInputNetwork.py
@@ -1,98 +1,119 @@
-from sklearn.model_selection import train_test_split
 import h5py
-import numpy as np
 from tensorflow import keras
-import time
-from anomalyDetection.miniCICADA.scripts.miniCICADAScripts.createMiniCICADAOneArray import loadExistingTrainValTestArrays
 import argparse
+import math
+import numpy as np
 
-def createTrainValTestArrays(inputFileForShuffle, outputFileForShuffle):
-    print('loading information...')
-    startTime = time.perf_counter()
-    with h5py.File(inputFileForShuffle) as inputFile:
-        cicadaInputArray = np.array(inputFile['cicadaInput'][:int(5.0e6)])
-        inputArray = np.array(inputFile['input'][:int(5.0e6)])
-    endTime = time.perf_counter()
-    print('Loaded!')
-    print(f'Loaded in {endTime-startTime:.2f} seconds.')
+class CICADAInputSequence(keras.utils.Sequence):
+    def __init__(self, fileName, batchSize, startEntry, endEntry):
+        self.fileName = fileName
+        self.batchSize = batchSize
+        self.startEntry = startEntry
+        self.endEntry = endEntry
 
-    print('Shuffling and splitting')
-    startTime = time.perf_counter()
-    train_val_input, test_input, train_val_output, test_output = train_test_split(inputArray, cicadaInputArray, train_size=0.9, random_state=1234)
+        self.hdf5File = h5py.File(self.fileName, 'r')
 
-    train_input, val_input, train_output, val_output = train_test_split(train_val_input, train_val_output, train_size=0.7, random_state=1234)
-    endTime=time.perf_counter()
-    print(f'Shuffled and split in {endTime-startTime:.2f} seconds')
+        self.totalBatches = (self.endEntry-self.startEntry) // self.batchSize
 
-    print('Writing file')
-    startTime = time.perf_counter()
-    with h5py.File(outputFileForShuffle, 'w') as outputFile:
-        outputFile.create_dataset('train_input', train_input.shape)
-        outputFile.create_dataset('train_output', train_output.shape)
-        outputFile.create_dataset('val_input', val_input.shape)
-        outputFile.create_dataset('val_output', val_output.shape)
-        outputFile.create_dataset('test_input', test_input.shape)
-        outputFile.create_dataset('test_output', test_output.shape)
+        #self.batchShape = self.hdf5File['input'][0].shape
+        firstElectronBatch = self.hdf5File['electrons'][0]
+        firstJetBatch = self.hdf5File['jets'][0]
+        firstTauBatch = self.hdf5File['taus'][0]
+        firstSumBatch = self.hdf5File['EtSums'][0]
+        theArray = np.append(firstElectronBatch, firstJetBatch)
+        theArray = np.append(theArray, firstTauBatch)
+        theArray = np.append(theArray, firstSumBatch)
+        self.batchShape = theArray.shape
+        
+        self.outputShape = self.hdf5File['cicadaInput'][0].shape
+    
+    def __len__(self):
+        return self.totalBatches
 
-        outputFile['train_input'][:] = train_input[:]
-        outputFile['train_output'][:] = train_output[:]
-        outputFile['val_input'][:] = val_input[:]
-        outputFile['val_output'][:] = val_output[:]
-        outputFile['test_input'][:] = test_input[:]
-        outputFile['test_output'][:] = test_output[:]
+    def __getitem__(self, index):
+        start = self.startEntry + (index * self.batchSize)
+        end = self.startEntry + ((index+1) * self.batchSize)
 
-    endTime = time.perf_counter()
-    print(f'Written in {endTime-startTime:.2f} seconds')
+        #electronData = self.hdf5File['input'][start:end]
+        batch_data = self.hdf5File['electrons'][start:end]
+        batch_data = np.append(batch_data, self.hdf5File['jets'][start:end], axis=1)
+        batch_data = np.append(batch_data, self.hdf5File['taus'][start:end], axis=1)
+        batch_data = np.append(batch_data, self.hdf5File['EtSums'][start:end], axis=1)
+        batch_output = self.hdf5File['cicadaInput'][start:end]
 
-    return train_input, train_output, val_input, val_output, test_input, test_output
+        return batch_data, batch_output
+
+    def on_epoch_end(self):
+        pass
+    
+    def __del__(self):
+        self.hdf5File.close()
 
 def main(args):
 
-    if args.loadExistingShuffle:
-        train_input, train_output, val_input, val_output, test_input, test_output = loadExistingTrainValTestArrays(
-            shuffledFile=args.shuffledFile,
-        )
-    else:
-        train_input, train_output, val_input, val_output, test_input, test_output = createTrainValTestArrays(
-            inputFileForShuffle=args.inputFileForShuffle,
-            outputFileForShuffle=args.shuffledFile,
-        )
+    with h5py.File(args.inputFile) as theFile:
+        totalEntries = len(theFile['cicadaInput'])
+
+    trainEntries = math.floor(0.7*totalEntries)
+    valEntries = math.floor(0.2*totalEntries)
+    testEntries = math.floor(0.1*totalEntries)
+
+    trainSequence = CICADAInputSequence(
+        fileName=args.inputFile,
+        batchSize=3200,
+        startEntry=0,
+        endEntry=trainEntries
+    )
+
+    valSequence = CICADAInputSequence(
+        fileName=args.inputFile,
+        batchSize=3200,
+        startEntry=trainEntries+1,
+        endEntry=trainEntries+1+valEntries
+    )
+
+    testEntries=CICADAInputSequence(
+        fileName=args.inputFile,
+        batchSize=3200,
+        startEntry=trainEntries+1+valEntries+1,
+        endEntry=totalEntries
+    )
 
     print('Input shapes:')
-    train_input_shape = f'Train: {train_input.shape}'
-    val_input_shape = f'Val: {val_input.shape}'
-    test_input_shape = f'Test: {test_input.shape}'
+    train_input_shape = f'Train: {len(trainSequence)} {trainSequence.batchShape}'
+    val_input_shape = f'Val: {len(valSequence)} {valSequence.batchShape}'
+    test_input_shape = f'Test: {len(testEntries)} {testEntries.batchShape}'
     print(f'{train_input_shape:^18} {val_input_shape:^18} {test_input_shape:^18}')
-    print('output_shapes:')  
-
-    train_output_shape = f'Train: {train_output.shape}'
-    val_output_shape = f'Val: {val_output.shape}'
-    test_output_shape = f'Test: {test_output.shape}'
+    print('output_shapes:')
+    
+    train_output_shape = f'Train: {len(trainSequence)} {trainSequence.outputShape}'
+    val_output_shape = f'Val: {len(valSequence)} {valSequence.outputShape}'
+    test_output_shape = f'Test: {len(testEntries)} {testEntries.outputShape}'
     print(f'{train_output_shape:^18} {val_output_shape:^18} {test_output_shape:^18}')
 
     model = keras.models.Sequential([
-        keras.layers.Input(shape=train_input.shape[1:]),
+        keras.layers.Input(shape=trainSequence.batchShape),
         keras.layers.BatchNormalization(),
-        keras.layers.Dropout(0.2),
+        # keras.layers.Dropout(0.2),
         
-        keras.layers.Dense(252),
+        keras.layers.Dense(1008),
         keras.layers.PReLU(),
         keras.layers.BatchNormalization(),
-        keras.layers.Dropout(0.2),
+        # keras.layers.Dropout(0.2),
 
-        keras.layers.Dense(504),
+        keras.layers.Dense(756),
         keras.layers.PReLU(),
         keras.layers.BatchNormalization(),
-        keras.layers.ActivityRegularization(l1=1e-3,l2=1e-3),
+        # keras.layers.ActivityRegularization(l1=1e-3,l2=1e-3),
         keras.layers.Dropout(0.2),
 
-        keras.layers.Reshape((18, 14, 2)),
+        keras.layers.Reshape((18, 14, -1)),
 
-        keras.layers.Conv2D(4, kernel_size=5, padding="same"),
+        keras.layers.Conv2D(2, kernel_size=7, padding="same"),
         keras.layers.PReLU(),
         keras.layers.LayerNormalization(),
 
-        keras.layers.Conv2D(8, kernel_size=3, padding="same"),
+        keras.layers.Conv2D(10, kernel_size=3, padding="same"),
         keras.layers.PReLU(),
         keras.layers.LayerNormalization(),
 
@@ -113,19 +134,23 @@ def main(args):
         monitor="val_loss",
         save_best_only = True,
     )
-    nanTermination = keras.callbacks.TerminateOnNaN()
+    # nanTermination = keras.callbacks.TerminateOnNaN()
+
+    earlyStop = keras.callbacks.EarlyStopping(
+        patience=6,
+    )
 
     try:
         model.fit(
-            x=train_input,
-            y=train_output,
-            epochs=10,
-            validation_data=(val_input, val_output),
-            steps_per_epoch = 2000,
-            validation_steps = 2000,
+            trainSequence,
+            epochs=500,
+            validation_data=valSequence,
+            # steps_per_epoch = 2000,
+            # validation_steps = 2000,
             callbacks = [
                 checkpointCallback,
-                nanTermination,
+                # nanTermination,
+                earlyStop,
             ]
         )
     except KeyboardInterrupt:
@@ -134,11 +159,7 @@ def main(args):
 
 if __name__ == '__main__':
     parser =argparse.ArgumentParser(description='Perform a miniCICADA training')
-    parser.add_argument('--performValidation', action='store_true',help='Perform the plot creation step immediately after training')
-    parser.add_argument('--loadExistingShuffle', action='store_true',help='Load existing split files, otherwise the will be created again and overwritten')
-    parser.add_argument('--histogramEntries',type=int,default=100000,help='Number of entries to test plots for')
-    parser.add_argument('--inputFileForShuffle',default='/nfs_scratch/aloeliger/testHDF5Files/largeInputFile.hdf5',help='file to load to shuffle')
-    parser.add_argument('--shuffledFile',default='/nfs_scratch/aloeliger/miniCICADATraining.hdf5',help='File to output for the shuffle, or to load from shuffle')
+    parser.add_argument('--inputFile', nargs='?', required=True, help='hdf5 file to build a sequence from')
 
     args = parser.parse_args()
     main(args)
